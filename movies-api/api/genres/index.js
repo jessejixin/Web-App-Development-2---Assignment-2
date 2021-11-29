734950ad914678b671e54f4c59f567822bd146db
Xin Ji <20096422@mail.wit.ie>
2021-11-29 12:25:56 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { genres } from './genresData';
+import Genre from './genreModel';
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres.genres);
+router.get('/', async(req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
