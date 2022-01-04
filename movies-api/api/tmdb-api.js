8e6064b3c194269837840f995ccbf06a67a4089a
Xin Ji <20096422@mail.wit.ie>
2022-01-04 14:33:10 +0000

    Get specific movie tested.

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -41,15 +41,7 @@ export const getMovieReviews = (id) => {
 export const getMovie = (id) => {
     return fetch(
        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}`
-    ).then((response) => {
-        if (!response.ok) {
-            throw new Error(response.json().message);
-        }
-        return response.json();
-    })
-        .catch((error) => {
-            throw error
-        });
+       ).then(res => res.json());
 };
 
 export const getMovieGenres = () => {
--- movies-api/tests/functional/api/movies/index.js
+++ movies-api/tests/functional/api/movies/index.js
@@ -4,19 +4,84 @@ import api from "../../../../index";  // Express API application
 
 
 const expect = chai.expect;
+const currentMovieId  = 24428
+const currentMovieTitle = "The Avengers"
+let newMovieId
+const newMovie = {
+  backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
+  genres: [
+    {
+      id: 14,
+      name: "Fantasy"
+    },
+    {
+      id: 12,
+      name: "Adventure"
+    },
+    {
+      id: 878,
+      name: "Science Fiction"
+    },
+    {
+      id: 28,
+      name: "Action"
+    }
+  ],        
+  id: 181808,
+  original_language: "en",
+  original_title: "Star Wars: The Last Jedi",
+  overview:
+    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+  popularity: 44.208,
+  poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  release_date: "2017-12-13",
+  tagline: "Darkness rises... and light to meet it",
+  title: "Star Wars: The Last Jedi",
+  video: false,
+  vote_average: 7,
+  vote_count: 9692
+};
 
 describe("Movies endpoint", () => {
-    describe("GET /movies ", () => {
-      it("should return movies and a status 200", (done) => {
-        request(api)
-          .get("/api/movies")
+  describe("GET /movies ", () => {
+    it("should return movies and a status 200", (done) => {
+      request(api)
+        .get("/api/movies")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).to.be.ok;
+          expect(res.body).to.be.a("object")
+          done();
+        });
+    });
+  });
+  describe("GET /movies/:id", () => {
+    describe("when the id is valid", () => {
+      it("should return the matching movie", () => {
+        return request(api)
+          .get(`/api/movies/${currentMovieId}`)
           .set("Accept", "application/json")
           .expect("Content-Type", /json/)
           .expect(200)
-          .end((err, res) => {
-            expect(res.body).to.be.ok;
-            done();
+          .then((res) => {
+            expect(res.body).to.have.property("title", currentMovieTitle);
           });
       });
     });
+    describe("when the id is invalid", () => {
+      it("should return the NOT found message", () => {
+        return request(api)
+        .get("/api/movies/xxx")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect({
+          success: false,
+          status_code: 34,
+          status_message: "The resource you requested could not be found.",
+        });
+    });
+   });
+  });
 });
